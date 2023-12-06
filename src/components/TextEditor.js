39f4ee2e5ec4f91ffeee00b6a00e7722a06fbaec
Barath <barath@Baraths-Mac-mini.local>
2023-12-06 17:41:11 +0530

    first commit

---
+++ src/components/TextEditor.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertToRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import draftToHtml from "draftjs-to-html";
+import html2pdf from "html2pdf.js";
+
+
+export default class TextEditor extends Component {
+  state = {
+    editorState: EditorState.createEmpty(),
+  };
+
+  onEditorStateChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+   pdfConverter = async () => {
+    const content = document.getElementById('content').value;
+    html2pdf(content, {
+      margin: 10,
+      filename: 'generated.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    });
+  };
+
+  render() {
+    const { editorState } = this.state;
+    console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+    return (
+      <div>
+        <button className="button" onClick={this.pdfConverter}>
+            Export
+          </button>
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={this.onEditorStateChange}
+        />
+        <textarea
+        id="content"
+          disabled
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+        ></textarea>
+      </div>
+    );
+  }
+}
